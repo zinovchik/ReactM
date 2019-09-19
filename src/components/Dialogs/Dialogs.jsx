7fb38813f225e7ecbac953db4d659f18a37c8329
Maxim Zinovchik <zinovchik.m@gmail.com>
2019-09-19 16:17:50 +0300

    Add message component in dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,65 +2,45 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
+
+const Dialogs_list_item = (props) => {
+    let path = '/dialogs/' + props.user_id;
+    return (
+        <NavLink to={path} activeClassName={s.active} className={s.dialogs_list_item}>
+            <img src={props.user_photo} />
+            <h5>{props.user_name}</h5>
+        </NavLink>
+    )
+}
+
+const Messages_list_item = (props)=>{
+    let clases = props.pos== 'my' ? s.messages_list_item + ' ' + s.left : s.messages_list_item + ' ' + s.right;
+    return (
+        <div className={clases}>
+            <img src={props.user_photo} />
+            <div className={s.wrap}>
+                <div className={s.name}>
+                    {props.user_name}
+                </div>
+                <div className={s.text}>
+                    {props.user_message}
+                </div>
+            </div>    
+        </div>
+    )
+}
 const Dialogs = (props) => {
     return (<div className={s.dialogs}>
         <div className={s.dialogs_list}>
-        <NavLink to="/dialogs/1" activeClassName={s.active} className={s.dialogs_list_item}>
-            <img src={'http://mythemestore.com/friend-finder/images/users/user-2.jpg'} />
-            <h5>Linda Lohan</h5>
-        </NavLink>   
-        <NavLink to="/dialogs/2" activeClassName={s.active}  className={s.dialogs_list_item}>
-            <img src={'http://mythemestore.com/friend-finder/images/users/user-10.jpg'} />
-            <h5>Julia Cox</h5>
-        </NavLink>
-            
+            <Dialogs_list_item user_id="1" user_name="Linda Lohan" user_photo="http://mythemestore.com/friend-finder/images/users/user-2.jpg"  />
+            <Dialogs_list_item user_id="1" user_name="Julia Cox" user_photo="http://mythemestore.com/friend-finder/images/users/user-10.jpg"  />
         </div>
         <div className={s.messages_list}>
-            <div className={s.messages_list_item+' '+s.left}>
-                <img src={'http://mythemestore.com/friend-finder/images/users/user-10.jpg'} />
-                <div className={s.wrap}>
-                    <div className={s.name}>
-                        Julia Cox
-                    </div>
-                    <div className={s.text}>
-                        Hello
-                    </div>
-                </div>    
-            </div>
-            <div className={s.messages_list_item+' '+s.left}>
-                <img src={'http://mythemestore.com/friend-finder/images/users/user-10.jpg'} />
-                <div className={s.wrap}>
-                    <div className={s.name}>
-                        Julia Cox
-                    </div>
-                    <div className={s.text}>
-                        How are you?
-                    </div>
-                </div>    
-            </div>
-
-            <div className={s.messages_list_item+' '+s.right}>
-                <img src={'http://mythemestore.com/friend-finder/images/users/user-2.jpg'} />
-                <div className={s.wrap}>
-                    <div className={s.name}>
-                        Linda Lohan
-                    </div>
-                    <div className={s.text}>
-                        Hello
-                    </div>
-                </div>    
-            </div>
-            <div className={s.messages_list_item+' '+s.right}>
-                <img src={'http://mythemestore.com/friend-finder/images/users/user-2.jpg'} />
-                <div className={s.wrap}>
-                    <div className={s.name}>
-                        Linda Lohan
-                    </div>
-                    <div className={s.text}>
-                        How are you?
-                    </div>
-                </div>
-            </div>
+            <Messages_list_item user_message="Hello" user_name="Linda Lohan" pos="my" user_photo="http://mythemestore.com/friend-finder/images/users/user-2.jpg"  />
+            <Messages_list_item user_message="How are you?" user_name="Linda Lohan" pos="my" user_photo="http://mythemestore.com/friend-finder/images/users/user-2.jpg"  />
+            <Messages_list_item user_message="Hi" user_name="Linda Lohan" pos="" user_photo="http://mythemestore.com/friend-finder/images/users/user-10.jpg"  />
+            <Messages_list_item user_message="excelent" user_name="Linda Lohan" pos="" user_photo="http://mythemestore.com/friend-finder/images/users/user-10.jpg"  />
+            
         </div>        
     </div>)
 }
