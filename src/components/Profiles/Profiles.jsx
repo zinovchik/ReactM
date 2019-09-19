76bb36196bcbc4e33fb55fc862b202ff0f8aeddc
Maxim Zinovchik <zinovchik.m@gmail.com>
2019-09-19 21:51:29 +0300

    add header to page Profiles

--- src/components/Profiles/Profiles.jsx
+++ src/components/Profiles/Profiles.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import s from './Profiles.module.css';
 
 const Profiles = () => {
-    return (<div>Main Content12</div>)
+    return (
+    <div className={s.profile}>
+        <div className={s.header}>
+            <img src="http://mythemestore.com/friend-finder/images/covers/1.jpg" className={s.picture} />
+            <div className={s.nav}></div>
+            <img src="http://mythemestore.com/friend-finder/images/users/user-1.jpg" className={s.photo} />
+        </div>
+        <div className={s.info}>
+            <div className={s.name}>Sarah Cruiz</div>
+        </div>
+
+    </div>)
 }
 
 export default Profiles;
